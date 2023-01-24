7adf98f2b3a6ad6c53815c18b81356e27dfeecb8
Nabil Hadi <napilfadl@gmail.com>
2023-01-24 10:08:32 +0200

    Add Game Coordinator logic

--- index.js
+++ index.js
@@ -37,6 +37,35 @@ function createElement({
   return elm;
 }
 
+const PubSub = (() => {
+  const events = [];
+  return {
+    subscribe(eventName, func) {
+      if (events[eventName]) {
+        events[eventName].push(func);
+        console.log(`${func.name} has subscribed to ${eventName} Topic!`);
+      } else {
+        events[eventName] = [func];
+        console.log(`${func.name} has subscribed to ${eventName} Topic!`);
+      }
+    },
+    unsubscribe(eventName, func) {
+      if (events[eventName]) {
+        events[eventName] = events[eventName].filter((fn) => fn !== func);
+        console.log(`${func.name} has unsubscribed from ${eventName} Topic!`);
+      }
+    },
+    publish(eventName, ...args) {
+      const funcs = events[eventName];
+      if (Array.isArray(funcs)) {
+        funcs.forEach((func) => {
+          func.apply(null, args);
+        });
+      }
+    },
+  };
+})();
+
 const Gameboard = (() => {
   let playerOneNumber = 1;
   let playerTwoNumber = 2;
@@ -93,10 +122,32 @@ const playerFactory = (id, name, number) => {
 const DisplayController = ((doc) => {
   const gameboardContainer = doc.querySelector(".gameboard-container");
 
+  const handleTileClick = (event) => {
+    const index = event.target.dataset.index;
+    if (!index) return;
+    PubSub.publish("tileClicked", { index });
+  };
+
+  const handleTilePlay = (payload) => {
+    const tile = gameboardContainer.querySelector(
+      `[data-index="${payload.index}"]`
+    );
+    if (!tile) return;
+    console.log(tile);
+
+    tile.textContent = payload.sign;
+  };
+
+  PubSub.subscribe("tilePlayed", handleTilePlay);
+
   const renderGameboard = (gameboard = []) => {
     gameboardContainer.innerHTML = "";
     gameboard.forEach((item, index) => {
-      const tile = createElement({ classNames: ["tile"], dataset: { index } });
+      const tile = createElement({
+        classNames: ["tile"],
+        dataset: { index },
+        eventHandlers: { click: handleTileClick },
+      });
       gameboardContainer.appendChild(tile);
     });
   };
@@ -105,3 +156,27 @@ const DisplayController = ((doc) => {
     renderGameboard,
   };
 })(document);
+
+const GameCoordinator = ((Gameboard, DisplayController) => {
+  let currentPlayer = 1;
+
+  const handleTileClickEvent = (payload = {}) => {
+    if (!payload.index) return;
+    let index = Number(payload.index);
+
+    if (!Gameboard.canPlayAt(index)) return;
+    Gameboard.playAt(index, currentPlayer);
+
+    PubSub.publish("tilePlayed", {
+      index,
+      sign: currentPlayer === 1 ? "X" : "O",
+    });
+    currentPlayer = currentPlayer === 1 ? 2 : 1;
+    // TODO
+    Gameboard.getWinner();
+    Gameboard.isDraw();
+  };
+  PubSub.subscribe("tileClicked", handleTileClickEvent);
+})(Gameboard, DisplayController);
+
+DisplayController.renderGameboard(Gameboard.getGameboardArray());
