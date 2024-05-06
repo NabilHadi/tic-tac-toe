9f5a69ccd33c3749f094bb97089431c6bf3a20a9
Nabil Hadi <42745851+NabilHadi@users.noreply.github.com>
2024-05-06 12:32:45 +0300

    Add Win condition

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const Gameboard = (() => {
     board[index].setValue(symbol);
   };
 
+  const isCellEmpty = (index) => {
+    if (board[index] == undefined) return;
+
+    return !board[index].getValue();
+  };
+
   const printBoard = () => {
     const b = board.map(cell => cell.getValue());
     console.log(`${b[0]}  ${b[1]}  ${b[2]}\n` +
@@ -23,10 +29,16 @@ const Gameboard = (() => {
     );
   };
 
+  const resetBoard = () => {
+    board.forEach(cell => cell.setValue(null));
+  };
+
   return {
     getBoard,
     setCellValue,
-    printBoard
+    printBoard,
+    resetBoard,
+    isCellEmpty,
   };
 })();
 
@@ -56,15 +68,18 @@ const GameController = ((
     {
       name: playerOneName,
       symbol: "X",
+      score: 0,
     },
     {
       name: playerTwoName,
       symbol: "O",
+      score: 0,
     }
   ];
 
 
   let activePlayer = players[0];
+  let remainingPlays = 9;
 
 
   const switchPlayerTurn = () => {
@@ -80,14 +95,67 @@ const GameController = ((
 
 
   const playRound = (index) => {
+    // Check if cell is playable
+    if (!Gameboard.isCellEmpty(index)) return;
+
     Gameboard.setCellValue(index, getActivePlayer().symbol);
     console.log(`${getActivePlayer().name} played at (${index})`);
 
+    remainingPlays--;
+    let winner = checkForWin();
+    if (winner.player) {
+      console.log(`winner: ${winner.player.name}`);
+      winner.player.score++;
+      if (winner.player.score >= 3) {
+        console.log(`Game over: ${winner.player.name} won the game!`);
+      } else {
+        startNewRound();
+      }
+    } else if (remainingPlays <= 0) {
+      console.log(`Draw!!`);
+      startNewRound();
+    }
     switchPlayerTurn();
     printNewRound();
   };
 
   printNewRound();
+  const winning = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  const checkForWin = () => {
+    let board = Gameboard.getBoard().map(cell => cell.getValue());
+    let indecies = winning.find(elm => {
+      if (board[elm[0]] === null || board[elm[1]] === null || board[elm[2]] === null) return;
+      if (board[elm[0]] === board[elm[1]] && board[elm[1]] === board[elm[2]]) return true;
+    });
+
+    let player = null;
+    if (indecies) {
+      player = indecies[0] === players[0].symbol ? players[0] : players[1];
+    }
+
+
+
+    return {
+      indecies,
+      player,
+    };
+  };
+
+  const startNewRound = () => {
+    Gameboard.resetBoard();
+    remainingPlays = 9;
+    console.log(`New Round started!`);
+  };
 
 
   return {
